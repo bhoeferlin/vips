56ff1d71c4d4cdddb1696aa6d38c52b98bab46ff
mameaw14 <pinyapornster@gmail.com>
2019-02-03 16:55:32 +0700

    Modify Tester to output HTML

--- src/main/java/org/fit/vips/VipsTester.java
+++ src/main/java/org/fit/vips/VipsTester.java
@@ -6,8 +6,12 @@
 
 package org.fit.vips;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * VIPS API example application.
+ * 
  * @author Tomas Popela
  *
  */
@@ -26,19 +30,32 @@ public class VipsTester {
 			System.exit(0);
 		}
 
-		String url = args[0];
-
+		
 		try
 		{
+			URL url = new URL(args[0]);
 			Vips vips = new Vips();
+
+			String dirName = "evinced-output-" + url.getHost().toString();
+			File theDir = new File(dirName);
+			try {
+				theDir.mkdir();
+			} catch (SecurityException e) {
+				System.err.println("Error: " + e.getMessage());
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+
 			// disable graphics output
 			vips.enableGraphicsOutput(false);
-			// disable output to separate folder (no necessary, it's default value is false)
-			vips.enableOutputToFolder(false);
 			// set permitted degree of coherence
 			vips.setPredefinedDoC(8);
+			
+			vips.setOutputFileName("VIPSResult");
+			vips.setOutputDirectoryName(dirName);
+
 			// start segmentation on page
-			vips.startSegmentation(url);
+			vips.startSegmentation(url.toString());
 		} catch (Exception e)
 		{
 			e.printStackTrace();
